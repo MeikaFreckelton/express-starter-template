1cd479c0763537de9c2abd99a0372667990bfc20
MeikaFreckelton <meikafreckelton@gmail.com>
2020-10-09 14:21:00 +1100

    completed challenge, working on extra challenge

--- utils/fetch_utilities.js
+++ utils/fetch_utilities.js
@@ -50,17 +50,19 @@ const fetchData = async () => {
     }
 }
 
-
+//  get all comments
 const getAllComments = function (req) {
     return Comment.find()
 }
 
-const getCommentsbySearch = function (req) {
-    
+// return comments based on the first letter of the body of the comment - by the params given
+const getCommentsBySearch = function (req) {
+    const letter = req.params.startingLetter.charAt(0)
+    return Comment.find({body: {$regex: '^' + letter, $options: 'i'}})
 }
 
 module.exports = {
     fetchData,
     getAllComments,
-    getCommentsbySearch
+    getCommentsBySearch
 }
